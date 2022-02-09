5d24e45c319d4eeb8f4356127cd009e859fdab37
Camilo Gómez <kamilo.sin.k@gmail.com>
2022-02-09 15:52:58 -0400

    Version 1

---
+++ src/components/Lista.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <ul class="list-group">
+            <li v-for="(item, index) of ordLista" :key="item.id" @click="aumentar(index)"
+            class="list-group-item d-flex justify-content-between align-items-center">
+                {{item.nombre}}
+                <span class="badge bg-primary rounded-pill">{{item.cantidad}}</span>
+            </li>
+        </ul>
+        <div class="d-grid gap-2">
+            <button class="btn btn-primary" type="button" @click="reset">Resetear</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    
+import {mapState, mapMutations} from 'vuex';
+
+export default {
+    name: 'Lista',
+    computed:{
+        ...mapState(['frutas']),
+        ordLista(state){
+            
+            return state.frutas.sort((a, b) => b.cantidad - a.cantidad);
+        }
+    },
+    methods:{
+        ...mapMutations(['aumentar', 'reset'])
+    }
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,21 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    frutas: [
+      {nombre: 'Manzana', cantidad: 0},
+      {nombre: 'Pera', cantidad: 0},
+      {nombre: 'Naranja', cantidad: 0}
+    ]
   },
   mutations: {
+    aumentar(state, index){
+      state.frutas[index].cantidad ++
+    },
+    reset(state){
+      state.frutas.forEach(i => {
+        i.cantidad = 0
+      })
+    }
   },
   actions: {
   },
